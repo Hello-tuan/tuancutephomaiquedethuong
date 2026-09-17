d047e9951901c91a49a38c07b8e1100bf05faa47
Hello-tuan <catmitomchuacay@gmail.com>
2026-09-17 13:25:33 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
-service xrdp start
+# Khởi động dbus
+service dbus start || true
 
+# Tạo thư mục X11
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
+# Khởi động PulseAudio (nếu cần)
+pulseaudio --start --system --disallow-exit --disable-shm 2>/dev/null || true
+
+# Khởi động XRDP
+service xrdp start
+
+# Giữ container chạy
 tail -f /var/log/xrdp-sesman.log
